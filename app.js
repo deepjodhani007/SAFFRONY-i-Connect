8bd1d565f36f37706378354161d76c23033109ea
deepjodhani007 <deepjodhani007@gmail.com>
2023-08-26 16:12:02 +0530

    API Creation

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const DB =
   "mongodb+srv://deepjodhani007:wFAWqgZIZoZSbvXV@cluster0.mktdbvt.mongodb.net/";
 
 //if we use local then it's get 3000 otherwise they automatically
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
--- model/user.js
+++ model/user.js
@@ -15,9 +15,45 @@ const NotificationSchema = mongoose.Schema({
     },
   });
 
+  const Login7CESchema = mongoose.Schema({
+    email: {
+      required: true,
+      type: String,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+    branch: {
+      required: true,
+      type: String,
+    },
+  });
+
+  const Login7ITSchema = mongoose.Schema({
+    email: {
+      required: true,
+      type: String,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+    branch: {
+      required: true,
+      type: String,
+    },
+  });
+
+
   const Notification = mongoose.model("Home Screen Data", NotificationSchema);
+  const Login7CE = mongoose.model("Userdata 7CE", Login7CESchema);
+  const Login7IT = mongoose.model("Userdata 7IT", Login7ITSchema);
+
 
   module.exports = {
     Notification,
+    Login7CE,
+    Login7IT,
   };
   
